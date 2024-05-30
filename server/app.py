4b10b9dbd37e58405939c9e5cc2c3ba547d03198
Joeeazy <joeeazy34@gmail.com>
2024-05-30 08:37:22 +0300

    entry point to the app server

--- server/app.py
+++ server/app.py
@@ -1,1 +1,24 @@
-print("Heloo")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+#initialize CORS
+CORS(app)
+
+# Config a database sqlite
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///circle_sync.db"
+
+# config for perfomance 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#create db instance
+db = SQLAlchemy(app)
+
+
+#run app
+if __name__ == "__main__":
+    app.run(debug=True)
